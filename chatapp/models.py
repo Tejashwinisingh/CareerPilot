44cca4030739cd3c6c58e4c458e540d3611742f0
Tejashwini Dhadekar <tejashwinidhadekar21@gmail.com>
2026-06-12 15:31:07 +0530

    Remove conflicting User model from chatapp

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-class User(AbstractUser):
-    pass
+# Create your models here. Define chat-specific models here, but do not
+# declare a `User` model in this app because `AUTH_USER_MODEL` is set to
+# `accounts.User`.
